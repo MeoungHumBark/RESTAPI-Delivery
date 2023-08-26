34dbadc020d0e78a5aa9b13c9cb2362eb4e7df4e
MeoungHum <emmabark84@gmail.com>
2023-08-26 22:39:09 +0900

    Constructor Added

--- FoodDeliveryAPI/src/main/java/FoodDeliveryApp/Food/Food.java
+++ FoodDeliveryAPI/src/main/java/FoodDeliveryApp/Food/Food.java
@@ -1,11 +1,11 @@
 package FoodDeliveryApp.Food;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 @Entity
 @Table(name="food")
--- FoodDeliveryAPI/src/main/java/FoodDeliveryApp/Food/FoodDTO.java
+++ FoodDeliveryAPI/src/main/java/FoodDeliveryApp/Food/FoodDTO.java
@@ -1,9 +1,12 @@
 package FoodDeliveryApp.Food;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
 @Builder
+@AllArgsConstructor
 @Getter
 @Setter
 public class FoodDTO {
